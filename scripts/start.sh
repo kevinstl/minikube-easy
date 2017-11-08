4b5dcb987ba09aad3ac6c69fa5d3a7f998ea14e4
kevin <kjwilde@gmail.com>
2017-11-08 08:30:11 -0600

    Added Jenkins installation.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 
 minikube start --vm-driver=xhyve --v=7 --alsologtostderr --cpus=4 --memory=12288 --disk-size=40g --insecure-registry localhost:5000 --extra-config=apiserver.Authentication.PasswordFile.BasicAuthFile=/var/lib/localkube/config/static-users.csv
 
-
+nohup minikube mount ~:/host-home &
