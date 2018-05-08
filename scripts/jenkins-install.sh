1354a3262a774e8d6c316af9b15f743eca394d14
kevin <kjwilde@hotmail.com>
2018-05-08 14:44:37 -0500

    Jenkins running locally with latest plugins.

--- scripts/jenkins-install.sh
+++ scripts/jenkins-install.sh
@@ -15,25 +15,22 @@ fi
 
 helm init
 
-#helm install --name jenkins --set Persistence.ExistingClaim=jenkins-host-home --set Master.NodePort=30010 stable/jenkins
-#helm install --name jenkins --set Persistence.ExistingClaim=jenkins --set Master.NodePort=30010 --set Master.RunAsUser=501 --set Master.FsGroup=501 stable/jenkins
-#helm install --name jenkins --set Master.NodePort=30010 --set Master.RunAsUser=501 --set Master.FsGroup=501 stable/jenkins
-#helm install --name jenkins --set Master.NodePort=30010 stable/jenkins
-#helm install --name jenkins --set Persistence.volumes=jenkins-host-home --set Master.NodePort=30010 stable/jenkins
-#helm install --name jenkins --set Master.NodePort=30010 --set Master.RunAsUser=501 --set Master.FsGroup=501 stable/jenkins
 
-#helm install --name jenkins --set Master.Image=jenkins/jenkins --set Master.ImageTag=lts --set Master.HealthProbesTimeout=600 --set Master.HealthProbeLivenessFailureThreshold=500 --set Persistence.ExistingClaim=jenkins-host-home --set Master.ServiceType=NodePort --set Master.NodePort=30010 stable/jenkins
-#helm install --name jenkins --set Persistence.ExistingClaim=jenkins-host-home --set Master.ServiceType=NodePort --set Master.NodePort=30010 stable/jenkins
+helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false \
+                --set Master.InstallPlugins[0]=kubernetes:1.6.0 --set Master.InstallPlugins[1]=workflow-aggregator:2.5 \
+                --set Master.InstallPlugins[2]=workflow-job:2.20 --set Master.InstallPlugins[3]=credentials-binding:1.16 \
+                --set Master.InstallPlugins[4]=git:3.8.0 \
+                -f jenkins-values.yml \
+                stable/jenkins
 
 
+#                --set Persistence.volumes[0].name=jenkins-host-home \
+#                --set Persistence.volumes[0].type=HostPath \
+#                --set Persistence.volumes[0].hostPath=\/host-home \
 
-#helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false --set Master.InstallPlugins={kubernetes=1.1, workflow-aggregator=2.5, workflow-job=2.15, credentials-binding=1.13, git=3.6.4} stable/jenkins
-#helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false --set Master.InstallPlugins[0].kubernetes=1.1 --set Master.InstallPlugins[0].workflow-aggregator=2.5 --set Master.InstallPlugins[0].workflow-job=2.15 stable/jenkins
-#helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false --set Master[0].InstallPlugins[0].kubernetes=1.1,Master[0].InstallPlugins[0].workflow-aggregator=2.5,Master[0].InstallPlugins[0].workflow-job=2.15,Master[0].InstallPlugins[0].credentials-binding=1.13,Master[0].InstallPlugins[0].git=3.6.4 stable/jenkins
-#helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false --set Master.InstallPlugins={kubernetes:1\.1, workflow\-aggregator:2\.5, workflow\-job:2\.15, credentials\-binding:1\.13, git:3\.6\.4} stable/jenkins
-#helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false --set Master.InstallPlugins[0]=kubernetes:1.1 --set Master.InstallPlugins[1]=workflow-aggregator:2.5 --set Master.InstallPlugins[2]=workflow-job:2.15 --set Master.InstallPlugins[3]=credentials-binding:1.13 --set Master.InstallPlugins[4]=git:3.6.4 stable/jenkins
-#helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false --set Master.InstallPlugins[0]=kubernetes:1.6.0 --set Master.InstallPlugins[1]=workflow-aggregator:2.5 --set Master.InstallPlugins[2]=workflow-job:2.15 --set Master.InstallPlugins[3]=credentials-binding:1.13 --set Master.InstallPlugins[4]=git:3.6.4 stable/jenkins
-helm install --name jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30010 --set Master.UseSecurity=false --set Master.InstallPlugins[0]=kubernetes:1.6.0 --set Master.InstallPlugins[1]=workflow-aggregator:2.5 --set Master.InstallPlugins[2]=workflow-job:2.20 --set Master.InstallPlugins[3]=credentials-binding:1.16 --set Master.InstallPlugins[4]=git:3.8.0 stable/jenkins
+
+#                --set Persistence.volumes[0].hostPath=\/host-home \
+#                --set Persistence.volumes[0].type=HostPath,Persistence.volumes[0].hostPath=/host-home,Persistence.volumes[0].mountPath=/host-home \
 
 #./stop.sh
 #./start.sh
--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,8 @@
 minikube start --vm-driver=hyperkit --v=7 --alsologtostderr --cpus=4 --memory=12288 --disk-size=40g --insecure-registry localhost:5000
 #minikube start --vm-driver=hyperkit --v=7 --alsologtostderr --cpus=4 --memory=12288 --disk-size=40g --insecure-registry localhost:5000
 
-mkdir /minikube
-chown 777 /minikube
+#mkdir /minikube
+#chown 777 /minikube
+#nohup minikube mount /minikube:/host-home &
 
-nohup minikube mount /minikube:/host-home &
+nohup minikube mount ~:/host-home &
