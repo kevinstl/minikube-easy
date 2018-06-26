a2da4663babee36e9a1f4383d458279ea8155251
kevin <kjwilde@hotmail.com>
2018-06-25 21:38:57 -0500

    Added debug message to understand what url smoketest is hitting.

--- src/test/java/smoke/SmokeTests.java
+++ src/test/java/smoke/SmokeTests.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -22,22 +24,26 @@ import static org.assertj.core.api.BDDAssertions.then;
 @SpringBootTest(classes = SmokeTests.class,
 		webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @EnableAutoConfiguration
+@PropertySources({
+	@PropertySource("classpath:properties/test.properties")
+})
 public class SmokeTests {
 
 	private static final Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass());
 
 	@Value("${stubrunner.url}") String stubRunnerUrl;
-	@Value("${application.url}") String applicationUrl;
+//	@Value("${application.url}") String applicationUrl;
+	@Value("${readme.url}") String readmeUrl;
 
 	TestRestTemplate testRestTemplate = new TestRestTemplate();
 
 	@Test
 	public void shouldWork() {
 
-		System.out.println("applicationUrl: " + applicationUrl);
+		System.out.println("applicationUrl: " + readmeUrl);
 
 		ResponseEntity<String> entity = this.testRestTemplate
-				.getForEntity("http://" + this.applicationUrl + "/", String.class);
+				.getForEntity("http://" + this.readmeUrl + "/", String.class);
 
 		then(entity.getStatusCode().is2xxSuccessful()).isTrue();
 	}
